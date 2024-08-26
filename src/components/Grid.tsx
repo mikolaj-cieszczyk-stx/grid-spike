c5e8eb673bf09884173cc060d6183eba22ad3cfe
Mikołaj Cięszczyk <mikolaj.cieszczyk@gmail.com>
2024-08-26 08:17:36 +0200

    fix

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FixedSizeList as List } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
+export { v4 as uuidv4 } from 'uuid';
 
 export type Option = {
   id: string;
