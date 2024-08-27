8d6fa863bd5ad8196090ad623bdeba0d8a83db65
Mikołaj Cięszczyk <mikolaj.cieszczyk@gmail.com>
2024-08-27 10:07:26 +0200

    revert

--- src/components/SelectField.tsx
+++ src/components/SelectField.tsx
@@ -15,22 +15,15 @@ export const SelectField = <T,>({
     { id: 'cat3', label: 'Category 3' },
   ];
 
-  const value = item[col.key];
-
-  // Znajdź obiekt Option na podstawie id, jeśli value jest id
-  const selectedOption =
-    typeof value === 'string'
-      ? options.find((opt) => opt.id === value)
-      : (value as Option | undefined);
+  const value = item[col.key] as Option | undefined;
 
   return (
     <select
-      value={selectedOption?.id || ''}
+      value={value?.id || ''}
       onChange={(e) => {
-        const newValue = e.target.value;
-        const newOption = options.find((opt) => opt.id === newValue);
-        if (onChange && newOption) {
-          onChange(item.id, col.key, newOption as T[keyof T]);
+        const selectedOption = options.find((opt) => opt.id === e.target.value);
+        if (onChange && selectedOption) {
+          onChange(item.id, col.key, selectedOption as T[keyof T]);
         }
       }}
       className="border w-full h-full"
